bbc5d22353719a04ec1c2b7f94bfda49ca75e95a
Ana Julia Ferreira Rodrigues <ana.jf.rodrigues.pessoal@gmail.com>
2026-05-28 08:26:57 -0300

    docs: adiciona documentação Swagger na rota de filtrar os pacientes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const routes = express.Router();
 
-// Importamos o Controller que acabamos de criar
+// controller
 const SinalVitalController = require('../controllers/SinalVitalController');
+
+/**
+ * @swagger
+ * /vitals/{babyId}:
+ *   get:
+ *     summary: Filtra sinais vitais por ID do bebê
+ *     description: Retorna a lista de batimentos e temperatura de um prematuro específico.
+ *     parameters:
+ *       - in: path
+ *         name: babyId
+ *         required: true
+ *         description: "O identificador do bebê (ex: Prematuro_01)"
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Sucesso ao retornar dados.
+ *       404:
+ *         description: Bebê não encontrado.
+ */
+routes.get('/vitals/:babyId', SinalVitalController.show);
 
 // Rota para o ESP32 enviar dados
 routes.post('/vitals', SinalVitalController.store);
 
-// Rota para a ONG/Mãe ver os dados
+// Rota para a ONG/Mãe ver todos os dados
 routes.get('/vitals', SinalVitalController.index);
-
-// Rota para buscar por um ID específico
-routes.get('/vitals/:babyId', SinalVitalController.show);
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const conectarDB = require('./config/db');
-const routes = require('./routes/routes'); 
+const routes = require('./routes/routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 require('dotenv').config();
 
 const app = express();
@@ -12,10 +14,30 @@ conectarDB();
 app.use(cors());
 app.use(express.json());
 
-// Usa as rotas definidas no arquivo routes.js
+// --- CONFIGURAÇÃO DO SWAGGER ---
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'NeoVínculo API',
+      version: '1.0.0',
+      description: 'Documentação da API de monitoramento de sinais vitais para a ONG Universo Prematuro',
+      contact: { name: 'Ana Júlia - Backend Lead' }
+    },
+    servers: [{ url: 'http://localhost:3000', description: 'Servidor Local' }],
+  },
+  apis: ['./src/routes/*.js'], 
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// --- ROTAS ---
 app.use(routes);
 
+// --- INICIALIZAÇÃO DO SERVIDOR ---
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(` NeoVínculo rodando na porta ${PORT}`);
+  console.log(`🚀 NeoVínculo rodando na porta ${PORT}`);
+  console.log(`📄 Documentação disponível em http://localhost:${PORT}/api-docs`);
 });
